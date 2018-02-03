b2143e5480ef76da8e7d85af6d50f135f640d6a1
folkert89 <folkertburghout@hotmail.com>
2018-02-03 17:38:46 +0100

    context B and C in green

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -4,6 +4,7 @@ class Codebreaker
 
     def initialize(output)
       @output = output
+      @feedback = ""
     end
 
     def start(secret_number)
@@ -17,7 +18,27 @@ class Codebreaker
     def guess(input)
       if input.length != 4
         output.puts "Try guessing a number with four digits"
+      else
+        loop_answer(input)
       end
     end
+
+    def loop_answer(input)
+      input.split("").each_with_index do |number, index|
+        if @secret_number.split("").include? number
+          check_answer(number, index)
+        end
+      end
+      output.puts @feedback
+    end
+
+    def check_answer(number, index)
+      if @secret_number[index] == number
+        @feedback += "+"
+      else
+        @feedback += "-"
+      end
+    end
+
   end
 end
